6ec94172cb5ee612c7c0bbe935dfdc4168c7aabe
Romario Stani <staniromario@gmail.com>
2024-08-18 14:48:40 +0000

    added a test and a workflow

---
+++ tests/test_1.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from src.main import app
+import re
+
+client = TestClient(app)
+
+def test_execute_command():
+    data = {
+        "ip": "192.168.0.1",
+        "command": "show ip interface brief",
+        "username": "admin",
+        "password": "password"
+    }
+    response = client.post("/execute-command/", json=data)
+    assert response.status_code == 200
+    assert "job_id" in response.json()
+    assert re.match(r"\d+", response.json()["job_id"])
